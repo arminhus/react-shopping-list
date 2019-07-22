d4e7dba349de191335d0caf53eb0a25dc2b499d0
Armin Husanovic <Armin@Armins-MacBook-Pro.local>
2019-07-21 20:09:10 -0400

    shopping list - adding, deleting, checking complete

--- src/App.js
+++ src/App.js
@@ -10,6 +10,39 @@ class App extends React.Component {
     ]
   };
 
+  handleAddItem = itemName => {
+    const newItems = [
+      ...this.state.shoppingItems,
+      { name: itemName, checked: false }
+    ];
+
+    this.setState({
+      shoppingItems: newItems
+    });
+    console.log("handle add item", { itemName });
+  };
+
+  handleDeleteItem = item => {
+    const newItems = this.state.shoppingItems.filter(itm => itm !== item);
+    this.setState({
+      shoppingItems: newItems
+    });
+    // console.log("handle delete item call", { item });
+  };
+
+  handleCheckItem = item => {
+    const newItems = this.state.shoppingItems.map(itm => {
+      if (itm === item) {
+        itm.checked = !itm.checked;
+      }
+      return itm;
+    });
+    this.setState({
+      shoppingItems: newItems
+    });
+    // console.log("handle check item call", { item });
+  };
+
   render() {
     return (
       <div>
@@ -18,10 +51,14 @@ class App extends React.Component {
         </header>
         <main>
           <section>
-            <AddItemForm />
+            <AddItemForm onAddItem={this.handleAddItem} />
           </section>
           <section>
-            <ShoppingList items={this.state.shoppingItems} />
+            <ShoppingList
+              items={this.state.shoppingItems}
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem}
+            />
           </section>
         </main>
       </div>
--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 class AddItemForm extends React.Component {
+  onSubmitForm = e => {
+    e.preventDefault();
+    this.props.onAddItem(e.target.itemToAdd.value);
+  };
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmitForm}>
         <input
+          name="itemToAdd"
           type="text"
           placeholder="carrots"
           aria-label="Shopping list item"
